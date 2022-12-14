1a48f49f3e6160bb0eefa899680c5ea6983d92d8
Silas Martins <silas.martins2041@gmail.com>
2022-12-14 18:09:38 -0300

    Página de Inscrição Responsiva

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -34,8 +34,8 @@ export function Subscribe() {
 
   return(
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="m-w-[640px]">
+      <div className="p-8 lg:p-0 block w-full max-w-[1100px] lg:flex items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]">
           <Logo />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
@@ -46,7 +46,7 @@ export function Subscribe() {
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="mt-10 lg:mt-0 p-8 bg-gray-700 border border-gray-500 rounded">
           <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
